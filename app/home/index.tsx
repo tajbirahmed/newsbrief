96f8675cb1858ddfbb5718003dda61b216facac4
tajbirahmed <tajbir26@gmail.com>
2024-06-21 12:31:07 +0600

    login, create account

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { View } from '@/components/Themed'
+import { Text } from '@/components/Themed'
 
 const Home = () => {
   return (
     <View>
           <Text>
-            abc
+            abctttttttttttt
             </Text> 
     </View>
   )
