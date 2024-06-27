483dad43490e27fee5049cd2024bfc5f2dca55a6
tajbirahmed <tajbir26@gmail.com>
2024-06-27 14:10:42 +0600

    article-view

--- screen/ArtcileViewScreen.tsx
+++ screen/ArtcileViewScreen.tsx
@@ -7,166 +7,205 @@ import { useLocalSearchParams } from 'expo-router'
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { Dimensions, Pressable, ScrollView } from 'react-native';
-import { MD2Colors } from 'react-native-paper';
+import { ActivityIndicator, MD2Colors, MD3Colors, Paragraph } from 'react-native-paper';
 import { useTailwind } from 'tailwind-rn';
-import  Constants  from 'expo-constants';
+import Constants from 'expo-constants';
 import { ChevronLeft } from 'lucide-react-native';
 import { exampleNews } from '@/constants/EXAMPLENEWS';
+import { DarkTheme } from '@react-navigation/native';
 
 
 const ArtcileViewScreen = () => {
 
-  const { article_id } = useLocalSearchParams<{
-    article_id: string
-  }>();
+	const { article_id } = useLocalSearchParams<{
+		article_id: string
+	}>();
 
-  const tw = useTailwind(); 
+	const tw = useTailwind();
 
-  const HEIGHT = Dimensions.get('window').height;
-  const WIDTH = Dimensions.get('window').width;
+	const HEIGHT = Dimensions.get('window').height;
+	const WIDTH = Dimensions.get('window').width;
 
-  const [articleLoading, setArticleLoading] = useState<boolean>(false)
-  const [article, setArticle] = useState<Result>(exampleNews);
+	const [articleLoading, setArticleLoading] = useState<boolean>(false)
+	const [article, setArticle] = useState<Result>(exampleNews);
 
-  const getArtcile = async () => { 
-    const articlesRef = collection(DB, 'Article');
-    const q = query(articlesRef,
-      where('article_id', '==', article_id)
-    );
-    const querySnapshot = await getDocs(q);
+	const getArtcile = async () => {
+		const articlesRef = collection(DB, 'Article');
+		const q = query(articlesRef,
+			where('article_id', '==', article_id)
+		);
+		const querySnapshot = await getDocs(q);
 
-    if (!querySnapshot.empty) {
-      const articleData = querySnapshot.docs[0].data() as Result;
-      setArticle(articleData);
-      // console.log("Article data:", articleData);
-    } else {
-      console.log("No such article!");
-      // setArticle(undefined);
-    }
-  }
-  function formatDate(dateString: string): string {
-    // Months array to convert month number to month name
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-      "Sep", "Oct", "Nov", "Dec"];
+		if (!querySnapshot.empty) {
+			const articleData = querySnapshot.docs[0].data() as Result;
+			setArticle(articleData);
+			// console.log("Article data:", articleData);
+		} else {
+			console.log("No such article!");
+			// setArticle(undefined);
+		}
+	}
+	function formatDate(dateString: string): string {
+		// Months array to convert month number to month name
+		const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+			"Sep", "Oct", "Nov", "Dec"];
 
-    // Split the input string into date and time parts
-    const [datePart, timePart] = dateString.split(' ');
-    const [year, month, day] = datePart.split('-').map(Number);
-    const [hour, minute] = timePart.split(':').map(Number);
+		// Split the input string into date and time parts
+		const [datePart, timePart] = dateString.split(' ');
+		const [year, month, day] = datePart.split('-').map(Number);
+		const [hour, minute] = timePart.split(':').map(Number);
 
-    // Format the date
-    const formattedDate = `${months[month - 1]} ${day}, ${year}, ${hour}:${minute}`;
+		// Format the date
+		const formattedDate = `${months[month - 1]} ${day}, ${year}, ${hour}:${minute}`;
 
-    return formattedDate;
-  }
-  const toUpper = (str: string): string => {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-  }
-  useEffect(() => { 
-    // getArtcile();
-  }, [])
+		return formattedDate;
+	}
+	const toUpper = (str: string): string => {
+		return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+	}
+	useEffect(() => {
+		// getArtcile();
+	}, [])
 
-  return (
-    <View style={[tw(""), {
-      flex: 1,
-      backgroundColor: MD2Colors.black,
-      height: Dimensions.get('window').height + 30,
-      position: 'absolute',
-      zIndex: 10,
-      minWidth: '100%',
+	return (
+		<View style={[tw(""), {
+			flex: 1,
+			backgroundColor: MD2Colors.black,
+			height: Dimensions.get('window').height + 30,
+			position: 'absolute',
+			zIndex: 10,
+			minWidth: '100%',
 
 
-    }]}
-    >
-      <ScrollView style={[tw("flex flex-col"), {
-        backgroundColor: 'transparent',
-        height: 'auto',
-        paddingBottom: 40,
-      }]}>
-        <Image
-          source={article?.image_url}
-          placeholder={BLURHASH}
-          style={{
-            width: WIDTH,
-            height: HEIGHT * 0.30,
-          }}
-        />
+		}]}
+		>
+			{articleLoading
+				?
+				(
+					<View style={[tw("flex flex-col justify-center self-center"), {
+						backgroundColor: 'transparent',
+						height: Dimensions.get('window').height,
+						width: '100%',
+						gap: 8,
+					}]}>
+						<ActivityIndicator
+							theme={DarkTheme}
+							color={MD2Colors.blue800}
+							animating={true}
+							style={tw("self-center")} />
+						<Text style={[tw("self-center"), { color: MD2Colors.blue800 }]}>
+							Fetching data....
+						</Text>
+					</View>
+				)
+				:
+				(
+					<ScrollView style={[tw("flex flex-col"), {
+						backgroundColor: 'transparent',
+						height: 'auto',
+						paddingBottom: 40,
+					}]}>
+						<Image
+							source={article?.image_url}
+							placeholder={BLURHASH}
+							style={{
+								width: WIDTH,
+								height: HEIGHT * 0.30,
+							}}
+						/>
 
-        <View style={[tw("absolute "), {
-          top: Constants.statusBarHeight + 5,
-          left: 10,
-          backgroundColor: 'transparent', 
-        }]}>
-          <ChevronLeft size={32} color={MD2Colors.white}  />
-        </View>
+						<View style={[tw("absolute "), {
+							top: Constants.statusBarHeight + 5,
+							left: 10,
+							backgroundColor: 'transparent',
+						}]}>
+							<ChevronLeft size={32} color={MD2Colors.white} />
+						</View>
 
-        <View style={[tw("flex flex-col"), {
-          padding: 10,
-        }]}>
-          <View style={[tw(""), {
-            
-          }]}>
-            <Text style={[tw("font-bold text-xl"), {
-              
-            }]}>
-              {article.title}
-            </Text>
-          </View>
-          <Pressable style={{
-            marginTop: 10,
-          }}
-            onPress={() => {
+						<View style={[tw("flex flex-col"), {
+							padding: 15,
+						}]}>
+							<View style={[tw(""), {
 
-            }}
-          >
-            <Text style={[tw(""), {
-              fontWeight: 'semibold',
-              fontSize: 14,
-            }]}>
-              {article.creator + " "}{"/ Updeated: " + formatDate(article.pubDate)}
-            </Text>
-          </Pressable>
+							}]}>
+								<Text style={[tw("font-bold text-xl"), {
 
-          <View style={[tw("flex flex-row "), {
-            overflow: 'visible',
-            flexWrap: 'wrap',
-          }]}>
-            <Text style={[tw("self-center font-semibold"), {
-              paddingRight: 6, 
-            }]}>
-              Tags:
-            </Text>
-            {article.category.map((category, index) => (
-              <View
-                key={index}
-                style={[tw("flex flex-row items-center justify-around"), {
-                  backgroundColor: '#00B0F0',
-                  borderRadius: 6,
-                  height: 'auto',
-                  width: 'auto',
-                  padding: 2,
-                  paddingHorizontal: 10,
-                  margin: 4,
-                  borderColor: 'white',
-                  borderWidth: 1,
-                }]}
+								}]}>
+									{article.title}
+								</Text>
+							</View>
+							<Pressable style={{
+								marginTop: 10,
+							}}
+								onPress={() => {
 
-              >
-                <Text
-                  key={index}
-                  style={[tw("text-white font-bold"), {}]}
-                >
-                  {toUpper(category)}
-                </Text>
-              </View>
-            ))}
-          </View>
+								}}
+							>
+								<Text style={[tw(""), {
+									fontWeight: 'semibold',
+									fontSize: 14,
+								}]}>
+									{article.creator + " "}{"/ Updeated: " + formatDate(article.pubDate)}
+								</Text>
+							</Pressable>
 
-        </View>
-        
-      </ScrollView>
-      </View>
-  )
+							<View style={[tw("flex flex-row "), {
+								overflow: 'visible',
+								flexWrap: 'wrap',
+							}]}>
+								<Text style={[tw("self-center font-semibold"), {
+									paddingRight: 6,
+								}]}>
+									Tag{article.category.length > 1 ? "s" : ""}:
+								</Text>
+								{article.category.map((category, index) => (
+									<View
+										key={index}
+										style={[tw("flex flex-row items-center justify-around"), {
+											backgroundColor: MD3Colors.primary20,
+											borderRadius: 6,
+											height: 'auto',
+											width: 'auto',
+											padding: 2,
+											paddingHorizontal: 10,
+											margin: 4,
+											borderColor: 'white',
+											borderWidth: 1,
+										}]}
+
+									>
+										<Text
+											key={index}
+											style={[tw("text-white font-bold"), {}]}
+										>
+											{toUpper(category)}
+										</Text>
+									</View>
+								))}
+							</View>
+							<View>
+								<Paragraph style={{
+									color: 'white', 
+									marginTop: 15, 
+								}}>
+									{"    "+article?.description}
+								</Paragraph>
+							</View>
+							{article.video_url
+								?
+								<></>
+								:
+								<></>
+							}
+
+						</View>
+
+					</ScrollView>
+				)
+
+			}
+		</View>
+	)
 }
 
 export default ArtcileViewScreen
